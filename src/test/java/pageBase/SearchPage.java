ac328c78a7c0a7ef535adf4357ef8c6f842cc0c3
SuelenWagner <siwc94@gmail.com>
2021-09-09 20:53:53 -0300

    test: added elements's actions in the class SearchPage

---
+++ src/test/java/pageBase/SearchPage.java
@@ -0,0 +1,52 @@
+package pageBase;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import pageObjects.SearchObjects;
+import utils.BasicConfiguration;
+
+public class SearchPage extends BasicConfiguration {
+
+    public SearchPage(WebDriver driver) {
+        super(driver);
+    }
+
+    SearchObjects pageObjects = new SearchObjects(driver);
+
+    public SearchPage accessHelpCenter() {
+        pageObjects.getBtnHelp().click();
+        pageObjects.getBtnHelpCenter().click();
+        waitTo(2);
+        return this;
+    }
+
+    public SearchPage searchProduct(String productName) {
+        pageObjects.getFieldSearch().sendKeys(productName);
+        waitTo(1);
+        return this;
+    }
+
+    public SearchPage accessMoreInformationOption() {
+        pageObjects.getSelectOptionForMoreInformation().click();
+        waitTo(2);
+        return this;
+    }
+
+    public SearchPage accessModalHowToAccessMySupergetAccount() {
+        pageObjects.getLinkSupergetAccountInfo().click();
+        pageObjects.getBtnCloseSupergetAccountInfo().click();
+        pageObjects.getModalHowToAccessMySupergetAccount().click();
+        waitTo(1);
+        return this;
+    }
+
+    public SearchPage validateModalHowToAccessMySupergetAccount(String labelSupergetAccount) {
+        Assert.assertEquals(labelSupergetAccount, pageObjects.getLabelHowToAccessMySupergetAccount().getText());
+        return this;
+    }
+
+    public SearchPage validateMessageNoResultsSearch(String messageNoResultsSearch) {
+        Assert.assertEquals(messageNoResultsSearch, pageObjects.getMessageNoResultsSearch().getText());
+        return this;
+    }
+}
